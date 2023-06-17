2fa15332ba3522e858ce3da4b9b18159c328b394
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2023-06-17 08:04:51 -0400

    Bug catch

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -23,7 +23,7 @@ def register():
         if error is None:
             try:
                 db.execute(
-                    f"INSERT INTO user (username, password) VALUES ({username}, {generate_password_hash(password)})",
+                    f"INSERT INTO user (username, password) VALUES (?, ?)", (username, generate_password_hash(password))
                 )
                 db.commit()
             except db.IntegrityError:
@@ -35,7 +35,7 @@ def register():
     
     return render_template('auth/register.html')
 
-@bp.register('/login', method=('GET', 'POST'))
+@bp.route('/login', methods=('GET', 'POST'),)
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -59,4 +59,32 @@ def login():
         flash(error)
     
     return render_template('auth/login.html')
-                
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
